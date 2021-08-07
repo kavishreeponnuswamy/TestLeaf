94f81155eb6ca1bdf3209d04e7b5407bcbfd25f2
kavishreeponnuswamy <67725464+kavishreeponnuswamy@users.noreply.github.com>
2021-08-07 19:25:00 +0530

    Add files via upload

---
+++ Week1/Day1/Classwork/LearnIfElseAndFor.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class LearnIfElseAndFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// print all even numbers from 1 to 50
+// For is done when we know the no of iterations
+		for (int i = 1; i <= 50; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
---
+++ Week1/Day1/Classwork/MobilePhone.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class MobilePhone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String brand = "Redmi";
+		char colour = 'G';
+		int price = 20000;
+		long imeiNo = 12345678910111213l;
+
+		float screenSize = 5.5f;
+		boolean isMobileWorking = true;
+
+		System.out.println("Mobile brand is " + brand + " of colour " + colour + " which is " + price + " rupees");
+		System.out.println("IMEI no is " + imeiNo);
+		System.out.println("It has screen size" + screenSize);
+		System.out.println("Is Mobile in working condition " + isMobileWorking);
+	}
+
+}
